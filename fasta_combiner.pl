1cc5a8053ac605ad82ef77bcb553887b02ad247a
Rob Long <robert.l.long@gmail.com>
2012-08-10 19:48:21 -0500

    completed fasta combiner

--- fasta_combiner.pl
+++ fasta_combiner.pl
@@ -5,4 +5,30 @@ use warnings;
 
 use IO::File;
 
+usage() unless @ARGV == 1;
 
+my $path = $ARGV[0];
+
+if ( not -d $path) {
+    usage();
+}
+
+#initialize chromosome/file list
+my @chr_list = (1..22,"X","Y","M");
+my @file_list = map { $_ = $path."/chr$_.fa";} @chr_list;
+
+for my $file (@file_list) {
+    my $fh = IO::File->new($file);
+    while ( my $line = $fh->getline) {
+        print $line;
+    }
+    $fh->close;
+}
+
+print @chr_list;
+
+sub usage {
+    print "\nusage:   ./fasta_combiner.pl <path/to/input/fastas>\n";
+    print "try>     ./fasta_combiner.pl /home/<yournamehere>/Downloads/ > myNewReference.fa\n";
+    exit(1);
+}
