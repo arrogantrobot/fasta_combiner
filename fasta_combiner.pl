d0ad751c41941ed2c681578fe8d001dff39c0a80
Rob Long <robert.l.long@gmail.com>
2012-08-11 13:00:05 -0500

    Added specific error message for missing inputs

--- fasta_combiner.pl
+++ fasta_combiner.pl
@@ -17,6 +17,9 @@ if ( not -d $path) {
 my @chr_list = (1..22,"X","Y","M");
 my @file_list = map { $_ = $path."/chr$_.fa";} @chr_list;
 
+#check for the presence of all the required files
+map { missing($_) if (not -e $_); } @file_list;
+
 #open each source and print its contents to stdout
 for my $file (@file_list) {
     my $fh = IO::File->new($file);
@@ -29,5 +32,11 @@ for my $file (@file_list) {
 sub usage {
     print "\nusage:   ./fasta_combiner.pl <path/to/input/fastas>\n";
     print "try>     ./fasta_combiner.pl /home/<yournamehere>/Downloads/ > myNewReference.fa\n";
+    exit(1);
+}
+
+sub missing {
+    my $file = shift;
+    print "\nCould not locate input at: $file\n";
     exit(1);
 }
